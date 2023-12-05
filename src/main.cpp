68852e5de8e3695f801eaee231c152631ea13186
Raul Quesada Navarro <raquenaengineering@gmail.com>
2023-12-06 00:17:49 +0100

    - implement laser ladder timed by potentiometer.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,20 @@
 
 // pin definitions //
 
+
 #define POT_PIN 32
-#define POT_POW_PIN 35
-#define POT_GND_PIN 33
+#define POT_POW_PIN 25
+#define POT_GND_PIN 26
 
 #define SWITCH_1_PIN 21
 #define SWITCH_2_PIN 22
 
 
 // USE 8 WIRE CABLE, 6 LASERS 2 GROUNDS
-#define LASERS_A_PIN 4
+
+#define LED_PIN 4
+
+#define LASERS_A_PIN 13
 #define LASERS_B_PIN 16
 #define LASERS_C_PIN 17
 #define LASERS_D_PIN 5
@@ -32,6 +36,12 @@ uint16_t potVal = 0;
 void updateSpeed(void);
 void triggerLasers(void);
 
+void allLasersOn(void);
+void allLasersOff(void);
+
+void  lasersOnOffMode(void);
+void  lasersBlinkLaderMode(void);
+
 
 void setup() {
 
@@ -40,13 +50,15 @@ void setup() {
   digitalWrite(POT_POW_PIN, HIGH);
 
   pinMode(POT_GND_PIN, OUTPUT);
-  digitalWrite(POT_POW_PIN, LOW);
+  digitalWrite(POT_GND_PIN, LOW);
 
   // pins for inputs //
 
   pinMode(POT_PIN, INPUT);
 
   // pins for outputs //
+
+  pinMode(LED_PIN,OUTPUT);
 
   pinMode(LASERS_A_PIN, OUTPUT);
   digitalWrite(LASERS_A_PIN, LOW);
@@ -74,6 +86,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   updateSpeed();
+  triggerLasers();
 
 
 }
@@ -82,23 +95,91 @@ void loop() {
 
 void updateSpeed(void) {
 
-  // static unsigned long t0 = millis();
+  static unsigned long dt = 500;          // period for this task
+  static unsigned long t0 = millis();
+  unsigned long t = millis();
 
-  potVal = analogRead(POT_PIN);
-  if(debugFlag == true){
-    Serial.print("potVal: ");
-    Serial.println(potVal);
+  if(t-t0 >= dt){
+    potVal = analogRead(POT_PIN);
+    if(debugFlag == true){
+      Serial.print("potVal: ");
+      Serial.println(potVal);
+      t0 = t;
+    }
   }
-
-  delay(10);
 
 }
 
 void triggerLasers(void){
+  // lasersOnOffMode();
+  lasersBlinkLaderMode();
 
-// basic all on / off //
+}
+
+
+void lasersOnoffMode(void){
+  // basic all on / off //
+  if(potVal > 2000){
+    // Serial.println("on");
+    allLasersOn();
+  }
+  else{
+    // Serial.println("off");
+    allLasersOff();
+  }
+}
+
+
+void  lasersBlinkLaderMode(void){
+  unsigned long dt = potVal >> 2;            // period for blink lader changes depending on pot val
+  static unsigned long t0 = millis();
+  unsigned long t = millis();
+
+  static uint8_t stateFlag = 0;            // number of stair in the lader (up to 4)
+
+  if(t-t0 >= dt){
+
+    if(stateFlag == 0){
+      digitalWrite(LASERS_A_PIN, HIGH);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+    }
+    if(stateFlag == 1){
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, HIGH);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+    }
+    if(stateFlag == 2){
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, HIGH);
+      digitalWrite(LASERS_A_PIN, LOW);
+    }
+    if(stateFlag == 3){
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, LOW);
+      digitalWrite(LASERS_A_PIN, HIGH);
+    }
+    Serial.println(stateFlag);
+    stateFlag = stateFlag + 1;
+    stateFlag = stateFlag % 4;
+
+    t0 = t;
+
+  }
 
 
 
+}
+
+
+void allLasersOn(void){
+  digitalWrite(LED_PIN, HIGH);
+}
+void allLasersOff(void){
+  digitalWrite(LED_PIN,LOW);
 
 }
